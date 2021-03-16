c82aeb12e63a5484f9bb52f1886a438823985be8
Logan Derrick  Intertek <logan.derrick@intertek.com>
2021-03-16 11:44:04 -0500

    Merged js changes locally

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(function () {
   $(".carousel").carousel({ interval: 2000 });
   $("#carouselButton").click(function () {
@@ -19,25 +18,3 @@ $(function () {
     $("#loginModal").modal("show");
   });
 });
-=======
-$(function(){
-    $(".carousel").carousel({interval: 2000});
-    $("#carouselButton").click(function(){
-      if ($("#carouselButton").children("i").hasClass("fa-pause")){
-        $(".carousel").carousel("pause");
-        $("#carouselButton").children("i").removeClass("fa-pause");
-        $("#carouselButton").children("i").addClass("fa-play")
-      } else {
-        $(".carousel").carousel("cycle");
-        $("#carouselButton").children("i").removeClass("fa-play");
-        $("#carouselButton").children("i").addClass("fa-pause")
-      }
-    });
-    $("#reserveButton").click(function(){
-      $("#reserveModal").modal("show");
-    });
-    $("#loginButton").click(function(){
-      $("#loginModal").modal("show");
-    });
-  });
->>>>>>> 3ec4e5821fec9f0e6fc9404c5641f9fad05de016
